6d61a252aa1a9524e20f01533579b2579e9cab55
Anarielle A <anarielle.a@gmail.com>
2022-03-12 16:35:06 +0300

    какая-то жесть

--- Notebook/Note.cs
+++ Notebook/Note.cs
@@ -8,5 +8,79 @@ namespace Notebook
 {
     internal class Note
     {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        private int phoneNumber;
+
+        public int PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Country { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Organization { get; set; }
+        public string Position { get; set; }
+        public int MyProperty { get; set; }
+        public string Notes { get; set; }
+
+        public void CreateNote()
+        {
+            Console.WriteLine("Введите фамилию:");
+            LastName = Console.ReadLine();
+            while (string.IsNullOrEmpty(LastName))
+            {
+                Console.WriteLine("Фамилия не введена, попробуйте ещё раз:");
+                LastName = Console.ReadLine();
+            }
+
+            Console.WriteLine("Введите имя:");
+            FirstName = Console.ReadLine();
+            while (string.IsNullOrEmpty(FirstName))
+            {                
+                Console.WriteLine("Имя не введено, попробуйте ещё раз:");
+                FirstName = Console.ReadLine();
+            }
+
+            Console.WriteLine("Введите номер телефона:");
+            bool tryParse = int.TryParse(Console.ReadLine(), out phoneNumber);
+            while (phoneNumber == 0 || tryParse);
+            {                
+                Console.WriteLine("Номер введен некорректно, попробуйте ещё раз:");
+                tryParse = int.TryParse(Console.ReadLine(), out phoneNumber);
+            }
+
+            Console.WriteLine("Введите страну:");
+            Country = Console.ReadLine();
+            while (string.IsNullOrEmpty(Country))
+            {
+                Console.WriteLine("Вы не ввели страну, попробуйте ещё раз:");
+                Country = Console.ReadLine();
+            }
+
+
+        }
+
+        public void EditNote()
+        {
+
+        }
+
+        public void RemoveNote()
+        {
+
+        }
+
+        public void ShowNote()
+        {
+
+        }
+
+        public void ShowAllNotes()
+        {
+
+        }
     }
 }
--- Notebook/Program.cs
+++ Notebook/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Notebook
+{
+    class Notebook
+    {
+        static void Main(string[] args)
+        {
+            List<Note> notes = new List<Note>();
+            Console.WriteLine("Выерите действие:\n1 - Добавить запись\n2 - Редактировать запись\n" +
+                "3 - Удалить запись\n4 - Просмотр записи\n5 - Просмотр всех записей");
+            Note note = new Note();
+            note.CreateNote();
+        }
+    }    
+}
